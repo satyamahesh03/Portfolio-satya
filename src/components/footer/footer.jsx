5f6ac7c0d50fd7deb92e127d33cd8e445fae0cdb
Satya Mahesh <satyamahesh.kolli03@gmail.com>
2025-05-15 13:57:23 +0530

    Updated footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import './footer.css';
-import {BsLinkedin} from 'react-icons/bs';
-import {FiGithub} from 'react-icons/fi';
-import {FaInstagram} from 'react-icons/fa';
+import { BsLinkedin } from 'react-icons/bs';
+import { FiGithub } from 'react-icons/fi';
+import { FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer>
-      <a href='#header' className='footer__logo' rel='noopener'>Satya Mahesh</a>
-      
-      <ul className='permalinks'>
-        <li><a href='#header'>Home</a></li>
-        <li><a href='#about'>About</a></li>
-        <li><a href='#skills'>skills</a></li>
-        <li><a href='#services'>Services</a></li>
-        <li><a href='#recentWork'>Projects</a></li>
-        <li><a href='#contact'>Contact</a></li>
-      </ul>
+      <a href="#header" className="footer__logo signature-style">Satya Mahesh</a>
 
       <div className='footer__socials'>
         <a href='https://www.linkedin.com/in/satyamaheshkolli'><BsLinkedin /></a>
